44c9fdf34f40dcb5255b355af6366fd7ae3f175a
Leonardo Dos Santos Quintania <leonardo.quintania@totvs.com.br>
2021-02-26 19:41:24 -0300

    Inseridos atualizações

--- hello_world.rb
+++ hello_world.rb
@@ -1,16 +1,17 @@
 # Isto é uma classe!
 class HelloWorld
-
 	# Construtor recebendo 'your_name' como parâmetro.
 	def initialize your_name
 		# Armazena o parâmetro em uma variável de instância.
     	@your_name = your_name
   	end
 
+	
 	# Método que imprime uma mensagem de boas vindas seguida de um nome.
   	def say_hello
     	puts "Bem vindo ao Ruby, #{@your_name}"
   	end
+
 end
 
 # Instancia a classe enviando meu nome como argumento para o construtor.
